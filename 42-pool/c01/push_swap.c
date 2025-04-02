3e39e2acd20f4a3c6f10b18325243028fd3072ae
legit_weirdo <aissiwassim@gmail.com>
2025-04-02 18:53:30 +0000

    THILINA HA DETTO DI NON TOCCARE NIENTE E PUSHARE IMMEDIATAMENTE

--- 42-pool/c01/push_swap.c
+++ 42-pool/c01/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: sara <sara@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/13 19:18:11 by sel-khao          #+#    #+#             */
-/*   Updated: 2025/04/02 18:21:01 by sara             ###   ########.fr       */
+/*   Updated: 2025/04/02 20:42:53 by sara             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,63 +50,97 @@ char	**split_arg(char **av)
 	return (mat);
 }
 
+int	two_args(t_ps **stack_a, char **argv)
+{
+	char	**matrix;
+	int	i;
+	int	n;
+
+	i = 0;
+	if (!argv[1][0])
+		return (-1);
+	if (!((argv[1][0] >= 0 && argv[1][0] <= 9) 
+	|| ((argv[1][0] == '-' || argv[1][0] == '+') && (argv[1][1] >= 0 && argv[1][1] <= 9))))
+		return(-1);
+	matrix = split_arg(argv);
+	if (!matrix)
+		return (-1);
+	while(matrix[i])
+	{
+		n = fft_atoi(matrix[i]);
+		fft_lstadd_back(stack_a, fft_lstnew(n));
+		i++;
+	}
+	if (i == 1)
+		return  (free_mat(matrix) ,fft_lstclear(stack_a), -1);
+	return (i);
+}
+
+int	more_args(t_ps **stack_a, char **argv, int argc)
+{
+	int	j;
+	int	n;
+	
+	j = 1;
+	if (checkdup(argc, argv) != 0)
+		return (-1);
+	while (j < argc)
+	{
+		n = fft_atoi(argv[j]);
+		fft_lstadd_back(stack_a, fft_lstnew(n));
+		j++;
+	}
+	return (0);
+}
+
+void	proccess(t_ps **stack_a, t_ps **stack_b, int i, int argc)
+{
+	if (argc == 3 || i == 2)
+		return (two_sort((stack_a)));
+	else if (argc == 4 || i == 3)
+		three_args(stack_a);
+	else if (argc <= 11 || i <= 10)
+		five_algo(stack_a, stack_b);
+	else
+		radix_sort(stack_a, stack_b);
+}
+
+int	last(t_ps **stack_a, t_ps **stack_b)
+{
+	if ((is_sorted((*stack_a)) != 1) || (*stack_b))
+	{
+		fft_lstclear(stack_a);
+		return (write(2, "Not sorted\n", 11), -1);
+	}
+	fft_lstclear(stack_a);
+	fft_lstclear(stack_b);
+	return (0);
+}
 int	main(int argc, char *argv[])
 {
 	t_ps	*stack_a;
 	t_ps	*stack_b;
-	int		j;
 	int		i;
-	int		n;
-	char	**matrix;
 	
-	if (argc < 2)
-	{
-		write(2, "Error\n", 6);
-		return (1);
-	}
-	i = 0;
 	stack_a = NULL;
 	stack_b = NULL;
-	matrix = NULL;
+	if (argc < 2)
+		return (write(2, "Error\n", 6), 1);
 	if (argc == 2)
 	{
-		matrix = split_arg(argv);
-		if (!matrix)
+		i = two_args(&stack_a, argv);
+		if (i == -1)
 			return (1);
-		while(matrix[i])
-		{
-			n = fft_atoi(matrix[i]);
-			fft_lstadd_back(&stack_a, fft_lstnew(n));
-			i++;	
-		}
-		if (i == 1)
-			return  (free_mat(matrix) ,fft_lstclear(&stack_a), 0);
 	}
 	else
-	{	
-		j = 1;
-		if (checkdup(argc, argv) != 0)
+	{
+		if (more_args(&stack_a, argv, argc) == -1)
 			return (1);
-		while (j < argc)
-		{
-			n = fft_atoi(argv[j]);
-			fft_lstadd_back(&stack_a, fft_lstnew(n));
-			j++;
-		}
 	}
 	if (is_sorted(stack_a) == 1)
 		return (0);
-	if (argc == 3 || i == 2)
-		return (two_sort((&stack_a)), 0);
-	if (argc == 4 || i == 3)
-		three_args(&stack_a);
-	else if (argc <= 11 || i <= 10)
-		five_algo(&stack_a, &stack_b);
-	else
-		radix_sort(&stack_a, &stack_b);
-	print_lst(stack_a);
-	if ((is_sorted(stack_a) != 1))
-		return (write(2, "Not sorted\n", 11), 1);
-	fft_lstclear(&stack_a);
+	proccess(&stack_a, &stack_b, i, argc);
+	if (last(&stack_a, &stack_b) == -1)
+		return (-1);
 	return (0);
 }
